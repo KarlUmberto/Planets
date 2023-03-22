e5e68bc7803d6236e4eb091f8848c9ceb62a0fb4
Karl Umberto Kats <karl.pongolini@gmail.com>
2023-03-22 16:41:55 +0200

    Add main content, main content scrollview, search bar

--- SolarPlanets/SolarPlanets/Views/PlanetsPage.xaml.cs
+++ SolarPlanets/SolarPlanets/Views/PlanetsPage.xaml.cs
@@ -1,9 +1,25 @@
+using Microsoft.Maui.Controls;
+
 namespace Views;
 
 public partial class PlanetsPage : ContentPage
 {
+	private const uint AnimationDuration = 800u;
 	public PlanetsPage()
 	{
 		InitializeComponent();
 	}
+
+	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+	
+	private async Task CloseMenu()
+	{
+		//close menu and shoq again main content
+		_ = MainContentGrid.FadeTo(1, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(1, AnimationDuration);
+        await MainContentGrid.TranslateTo(0,0, AnimationDuration, Easing.CubicIn);
+    }
 }
