f7d6653bdb1ccc3f40f78769fe53b66bcee9564c
Karl Umberto Kats <karl.pongolini@gmail.com>
2023-03-29 15:53:21 +0300

    Add PlanetsPage

--- SolarPlanets/SolarPlanets/Models/Planet.cs
+++ SolarPlanets/SolarPlanets/Models/Planet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Models
+namespace SolarPlanets.Models
 {
     public class Planet
     {
@@ -15,5 +15,26 @@ namespace Models
         public Microsoft.Maui.Graphics.Color AccentColorEnd { get; set; }
 
         public List<string> Images { get; set; }
+        
+        //Background
+        public Brush Backrgound
+        {
+            get
+            {
+                var gradientStops = new GradientStopCollection();
+                gradientStops.Add(new GradientStop(AccentColorStart, 0.0f));
+               gradientStops.Add(new GradientStop(AccentColorEnd, 1.0f));
+
+                var bgBrush = new LinearGradientBrush(gradientStops,
+                    new Point(0.0, 0.0),
+                    new Point(1.0, 1.0));
+
+                return bgBrush;
+
+            }
+        }
     }
+
+
+    
 }
